7526685efcc481f5c4935e8ddf22df54e0111030
audrius145 <58250824+audrius145@users.noreply.github.com>
2021-04-08 11:25:28 +0200

    Additional design changes

--- app/src/main/java/com/example/plantaseed/View/MainActivity.java
+++ app/src/main/java/com/example/plantaseed/View/MainActivity.java
@@ -1,44 +1,65 @@
 package com.example.plantaseed.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.plantaseed.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavController navController;
+    BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnav);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-
+        initViews();
+        setupNavigation();
+    }
+    private void initViews() {
+        bottomNavigationView = findViewById(R.id.bottomnav);
+        toolbar = findViewById(R.id.toolbar);
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
 
-                    if (item.getItemId() == R.id.nav_home) {
-                        selectedFragment = new HomeFragment();
-                    } else if (item.getItemId() == R.id.nav_plants) {
-                        selectedFragment = new PlantsFragment();
-                    } else {
-                        selectedFragment = new InfoFragment();
-                    }
+    private void setupNavigation()
+    {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+    }
 
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
 
-                    return true;
-                }
-            };
+
+
+    //    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+//            new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                @Override
+//                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                    Fragment selectedFragment = null;
+//
+//                    if (item.getItemId() == R.id.nav_home) {
+//                        selectedFragment = new HomeFragment();
+//                    } else if (item.getItemId() == R.id.nav_plants) {
+//                        selectedFragment = new PlantsFragment();
+//                    } else {
+//                        selectedFragment = new InfoFragment();
+//                    }
+//
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+//
+//                    return true;
+//                }
+//            };
 }
